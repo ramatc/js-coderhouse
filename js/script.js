0009554f45e1ba0cb942b5038ea73ba7e1c0ce1e
ramatc <rtanquiascornejo@gmail.com>
2021-07-06 01:35:57 -0300

    Agrego eventos, para las validaciones del formulario y para agregar productos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,132 @@
 window.addEventListener("load", function() {
     //Llamado a las sections principales del body, para insertarles los productos
     let mainSection = document.getElementsByClassName("main-article");
+
+    //Llamado al formulario y todos sus elementos para validarlo
+    let form = document.getElementById("form");
+    let name = document.getElementById("name");
+    let email = document.getElementById("mail");
+    let tel = document.getElementById("tel");
+    let msg = document.getElementById("msg");
+    let buttonSubmit = document.getElementById("button-submit");
+    
+    //Llamado a los divs de errores para agregar un mensaje al formulario
+    let erName = document.querySelector(".erName")
+    let erEmail = document.querySelector(".erEmail")
+    let erTel = document.querySelector(".erTel")
+    let erMsg = document.querySelector(".erMsg")
+
+    //Formato para validar emails
+    let mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+    name.addEventListener('blur',function(){
+        if(name.value == "" ){
+            name.classList.add('is-invalid'); 
+            name.classList.remove('is-valid');
+            erName.innerText = "Tienes que ingresar un nombre" 
+        } else if(name.value.length < 2){
+            name.classList.add('is-invalid'); 
+            name.classList.remove('is-valid');
+            erName.innerText = "Debes usar 2 caracteres o más" 
+        } else {
+            name.classList.remove('is-invalid');
+            name.classList.add('is-valid'); 
+            erName.innerText = ""
+        }
+    })
+
+    email.addEventListener('blur',function(){
+        if(email.value == "" ){
+            email.classList.add('is-invalid'); 
+            email.classList.remove('is-valid');
+            erEmail.innerText = "Tienes que ingresar un correo electronico" 
+        } else if(!email.value.match(mailFormat)){
+            email.classList.add('is-invalid');  
+            email.classList.remove('is-valid'); 
+            erEmail.innerText = "Debes ingresar un formato de correo valido"
+        } else {
+            email.classList.remove('is-invalid');
+            email.classList.add('is-valid');   
+            erEmail.innerText = ""
+        }
+    })
+
+    tel.addEventListener('blur',function(){
+        if(tel.value == "" ){
+            tel.classList.add('is-invalid'); 
+            tel.classList.remove('is-valid'); 
+            erTel.innerText = "Tienes que ingresar un número de telefono" 
+        } else if(tel.value.length < 8){
+            tel.classList.add('is-invalid'); 
+            tel.classList.remove('is-valid'); 
+            erTel.innerText = "Debes ingresar un número de telefono valido, con al menos 8 digitos" 
+        }else{
+            tel.classList.remove('is-invalid');
+            tel.classList.add('is-valid'); 
+            erTel.innerText = ""
+        }
+    })
+
+    msg.addEventListener('blur',function(){
+        if(msg.value == "" ){
+            msg.classList.add('is-invalid'); 
+            msg.classList.remove('is-valid'); 
+            erMsg.innerText = "Tienes que ingresar un mensaje" 
+        } else {
+            msg.classList.remove('is-invalid');
+            msg.classList.add('is-valid'); 
+            erMsg.innerText = ""
+        }
+    })
+
+    buttonSubmit.addEventListener(('click'),function(e){
+        e.preventDefault()
+        let errores = {};
+
+        if(name.value == ""){
+            errores.name = "Tienes que ingresar un nombre"
+            name.classList.add('is-invalid');  
+        }else if(name.value.length < 2){
+            errores.name = "Debes usar 2 caracteres o más" 
+            name.classList.add('is-invalid');  
+        };
+
+        if(email.value == ""){
+            errores.email = "Tienes que ingresar un correo electronico"
+            email.classList.add('is-invalid');  
+        } else {
+            if(!email.value.match(mailFormat)){
+                errores.email = "Debes ingresar un formato de correo valido"
+            }
+        };
+
+        if(tel.value == ""){
+            errores.tel = "Tienes que ingresar un número de telefono"
+            tel.classList.add('is-invalid');  
+        }else if(tel.value.length < 8){
+            errores.tel = "Debes ingresar un número de telefono valido, con al menos 8 digitos" 
+            tel.classList.add('is-invalid'); 
+        };
+
+        if(msg.value == ""){
+            errores.msg = "Tienes que ingresar un mensaje"
+            msg.classList.add('is-invalid');  
+        };
+    
+        if(Object.keys(errores).length >= 1){
+            erName.innerText = (errores.name) ? errores.name : ' ';
+            erEmail.innerText = (errores.email) ? errores.email : ' ';
+            erTel.innerText = (errores.tel) ? errores.tel : ' ';
+            erMsg.innerText = (errores.msg) ? errores.msg : ' ';
+           
+        } else {
+            form.submit();
+            alert("Mensaje enviado con exito");
+        }
+    })
+
+    //Declaracion de array vacio para agregar los productos que se sumen al carrito
+    const carrito = [];
   
     //Declaracion de array con el listado de productos 
     const products = [
@@ -11,7 +137,7 @@ window.addEventListener("load", function() {
     {id: 5, img:"xxxtentacion.jpg", name: "XXXTentacion", description: "BAD VIBES FOREVER CD", price: "850", class: "cd"},
     {id: 6, img:"kendrick.jpg", name: "Kendrick Lamar", description: "GOOD KID M.A.A.D CITY CD", price: "650", class: "cd"},
     {id: 7, img:"juice.jpg", name: "Juice WRLD", description: "DEATH RACE FOR LOVE CD", price: "500", class: "cd"},
-    {id: 8, img:"postmalone.jpg", name: "Post Malone", description: "HOLLYWOOD'S BLEEDING CD", price: "450", class: "cd"}];
+    {id: 8, img:"postmalone.jpg", name: "Post Malone", description: "HOLLYWOOD BLEEDING CD", price: "450", class: "cd"}];
 
     for (const product of products) {
         if (product.class === "vinyl") {
@@ -20,7 +146,7 @@ window.addEventListener("load", function() {
                                     <h3 class="title-product">${product.name}</h3>
                                     <p class="description-product">${product.description}</p>
                                     <p class="price-product">$${product.price}</p>
-                                    <button class="cart-button">Agregar al carrito<i class="fas fa-shopping-cart"></i></button>`;
+                                    <button class="cart-button">Agregar al carrito <i class="fas fa-shopping-cart"></i></button>`;
             mainSection[0].appendChild(contenedor);
         }else{
             let contenedor = document.createElement("article");
@@ -28,12 +154,17 @@ window.addEventListener("load", function() {
                                     <h3 class="title-product">${product.name}</h3>
                                     <p class="description-product">${product.description}</p>
                                     <p class="price-product">$${product.price}</p>
-                                    <button class="cart-button">Agregar al carrito<i class="fas fa-shopping-cart"></i></button>`;
+                                    <button class="cart-button">Agregar al carrito <i class="fas fa-shopping-cart"></i></button>`;
             mainSection[1].appendChild(contenedor);
         }
     }
- 
-    //Declaracion de array vacio para agregar los productos que se sumen al carrito
-    const carrito = [];
 
+    //Capturo los botones de "agregar al carrito" de cada producto, para luego iterarlos y enviar una alerta.
+    let boton = document.getElementsByClassName("cart-button");
+    for (let i = 0; i < boton.length; i++) {
+        boton[i].addEventListener("click",function(e) {
+            alert("Producto agregado al carrito!")
+        })
+    }
+   
 })
