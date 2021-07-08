c117b8af9e1c8e071ff8ce2def29c08eab5f2218
ramatc <rtanquiascornejo@gmail.com>
2021-07-08 19:45:28 -0300

    Comienzo con las funciones para el carrito

--- js/script.js
+++ js/script.js
@@ -1,6 +1,64 @@
 window.addEventListener("load", function() {
     //Llamado a las sections principales del body, para insertarles los productos
     let mainSection = document.getElementsByClassName("main-article");
+   
+    //Declaracion de array vacio para agregar los productos que se sumen al carrito
+    const carrito = [];
+
+    //Funcion para setear items en el local storage
+    function setLocal(key, value){
+        localStorage.setItem(key, value);
+    }
+    
+    //Declaracion de array con el listado de productos 
+    const products = [
+    {id: 1, img:"astroworld.jpg", name: "Travis Scott", description: "ASTROWORLD Vinyl Record", price: 2500, class: "vinyl"},
+    {id: 2, img:"igor.jpg", name: "Tyler, The Creator", description: "THE IGOR LP Vinyl Record", price: 1300, class: "vinyl"},
+    {id: 3, img:"take-care.jpg", name: "Drake", description: "TAKE CARE Vinyl Record", price: 1100, class: "vinyl"},
+    {id: 4, img:"slim-shady.jpg", name: "Eminem", description: "SLIM SHADY LP Vinyl Record", price: 1700, class: "vinyl"},
+    {id: 5, img:"xxxtentacion.jpg", name: "XXXTentacion", description: "BAD VIBES FOREVER CD", price: 850, class: "cd"},
+    {id: 6, img:"juice.jpg", name: "Juice WRLD", description: "DEATH RACE FOR LOVE CD", price: 500, class: "cd"},
+    {id: 7, img:"kendrick.jpg", name: "Kendrick Lamar", description: "GOOD KID M.A.A.D CITY CD", price: 650, class: "cd"},
+    {id: 8, img:"postmalone.jpg", name: "Post Malone", description: "HOLLYWOOD BLEEDING CD", price: 450, class: "cd"}];
+ 
+    for (const product of products) {
+        if (product.class === "vinyl") {
+            let contenedor = document.createElement("article");
+            contenedor.innerHTML = `<img src="img/${product.img}" alt="${product.description}">
+                                    <h3 class="title-product">${product.name}</h3>
+                                    <p class="description-product">${product.description}</p>
+                                    <p class="price-product">$${product.price}</p>
+                                    <button class="cart-button" id=${product.id}>Agregar al carrito <i class="fas fa-shopping-cart"></i></button>`;
+            mainSection[0].appendChild(contenedor);
+        }else{
+            let contenedor = document.createElement("article");
+            contenedor.innerHTML = `<img src="img/${product.img}" alt="${product.description}">
+                                    <h3 class="title-product">${product.name}</h3>
+                                    <p class="description-product">${product.description}</p>
+                                    <p class="price-product">$${product.price}</p>
+                                    <button class="cart-button" id=${product.id}>Agregar al carrito <i class="fas fa-shopping-cart"></i></button>`;
+            mainSection[1].appendChild(contenedor);
+        }
+    }
+ 
+    //Capturo los botones de "agregar al carrito" de cada producto.
+    let boton = document.getElementsByClassName("cart-button");
+    for (let i = 0; i < boton.length; i++) {
+        boton[i].addEventListener("click",function() {
+           addCart(boton[i].id);
+        })
+    }
+
+    function addCart(id){
+        let items = products.filter(product => product.id == id);
+        let item = items[0];
+        carrito.push({id: item.id, img: item.img, name: item.name, description: item.description, price: item.price});
+        setLocal("Carrito", JSON.stringify(carrito));
+    }
+
+    let carritoParseado = JSON.parse(localStorage.getItem("Carrito"));
+    console.log(carritoParseado);
+   
 
     //Llamado al formulario y todos sus elementos para validarlo
     let form = document.getElementById("form");
@@ -79,7 +137,7 @@ window.addEventListener("load", function() {
         }
     })
 
-    buttonSubmit.addEventListener(('click'),function(e){
+    buttonSubmit.addEventListener('click',function(e){
         e.preventDefault()
         let errores = {};
 
@@ -94,10 +152,9 @@ window.addEventListener("load", function() {
         if(email.value == ""){
             errores.email = "Tienes que ingresar un correo electronico"
             email.classList.add('is-invalid');  
-        } else {
-            if(!email.value.match(mailFormat)){
-                errores.email = "Debes ingresar un formato de correo valido"
-            }
+        }else if(!email.value.match(mailFormat)){
+            errores.email = "Debes ingresar un formato de correo valido"
+            email.classList.add('is-invalid'); 
         };
 
         if(tel.value == ""){
@@ -124,47 +181,27 @@ window.addEventListener("load", function() {
             alert("Mensaje enviado con exito");
         }
     })
-
-    //Declaracion de array vacio para agregar los productos que se sumen al carrito
-    const carrito = [];
-  
-    //Declaracion de array con el listado de productos 
-    const products = [
-    {id: 1, img:"astroworld.jpg", name: "Travis Scott", description: "ASTROWORLD Vinyl Record", price: "2500", class: "vinyl"},
-    {id: 2, img:"igor.jpg", name: "Tyler, The Creator", description: "THE IGOR LP Vinyl Record", price: "1300", class: "vinyl"},
-    {id: 3, img:"slim-shady.jpg", name: "Eminem", description: "SLIM SHADY LP Vinyl Record", price: "1700", class: "vinyl"},
-    {id: 4, img:"take-care.jpg", name: "Drake", description: "TAKE CARE Vinyl Record", price: "1100", class: "vinyl"},
-    {id: 5, img:"xxxtentacion.jpg", name: "XXXTentacion", description: "BAD VIBES FOREVER CD", price: "850", class: "cd"},
-    {id: 6, img:"kendrick.jpg", name: "Kendrick Lamar", description: "GOOD KID M.A.A.D CITY CD", price: "650", class: "cd"},
-    {id: 7, img:"juice.jpg", name: "Juice WRLD", description: "DEATH RACE FOR LOVE CD", price: "500", class: "cd"},
-    {id: 8, img:"postmalone.jpg", name: "Post Malone", description: "HOLLYWOOD BLEEDING CD", price: "450", class: "cd"}];
-
-    for (const product of products) {
-        if (product.class === "vinyl") {
-            let contenedor = document.createElement("article");
-            contenedor.innerHTML = `<img src="img/${product.img}" alt="${product.description}">
-                                    <h3 class="title-product">${product.name}</h3>
-                                    <p class="description-product">${product.description}</p>
-                                    <p class="price-product">$${product.price}</p>
-                                    <button class="cart-button">Agregar al carrito <i class="fas fa-shopping-cart"></i></button>`;
-            mainSection[0].appendChild(contenedor);
-        }else{
-            let contenedor = document.createElement("article");
-            contenedor.innerHTML = `<img src="img/${product.img}" alt="${product.description}">
-                                    <h3 class="title-product">${product.name}</h3>
-                                    <p class="description-product">${product.description}</p>
-                                    <p class="price-product">$${product.price}</p>
-                                    <button class="cart-button">Agregar al carrito <i class="fas fa-shopping-cart"></i></button>`;
-            mainSection[1].appendChild(contenedor);
-        }
-    }
-
-    //Capturo los botones de "agregar al carrito" de cada producto, para luego iterarlos y enviar una alerta.
-    let boton = document.getElementsByClassName("cart-button");
-    for (let i = 0; i < boton.length; i++) {
-        boton[i].addEventListener("click",function(e) {
-            alert("Producto agregado al carrito!")
-        })
-    }
-   
 })
+
+    //Capturo el boton que nos lleva arriba de todo.
+    buttonTop = document.getElementById("btnTop");
+
+    //Cuando el usuario scrollea 750px, aparece el boton
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+      if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
+        buttonTop.style.display = "block";
+      } else {
+        buttonTop.style.display = "none";
+      }
+    }
+
+    //Cuando el usuario clickea el boton, lo lleva hacia arriba de todo.
+    function topFunction() {
+      document.documentElement.scrollTop = 0; //Chrome, Firefox, Internet Explorer y Opera
+      document.body.scrollTop = 0; //Safari
+    }
+
+    buttonTop.addEventListener("click", topFunction);
+    
